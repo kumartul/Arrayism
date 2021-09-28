14744dd164014851d9b74867a7ed4070ba1a6c40
Atul Kumar <akumarofficial4224@gmail.com>
2021-09-28 11:44:13 +0530

    Update arrayism.js

--- arrayism.js
+++ arrayism.js
@@ -3,6 +3,9 @@ const clone = require('./src/clone.js');
 const get = require('./src/get.js');
 const clean = require('./src/clean.js');
 const pollute = require('./src/pollute.js');
+const unique = require('./src/unique.js');
+const remove = require('./src/remove.js');
+const math = require('./src/math.js');
 
 class Arrayism {
     constructor() {
@@ -11,6 +14,12 @@ class Arrayism {
         this.get = get;
         this.clean = clean;
         this.pollute = pollute;
+        this.unique = unique;
+        this.remove = remove;
+        this.sum = math.sum;
+        this.difference = math.difference;
+        this.product = math.product;
+        this.quotient = math.quotient;
     }
 }
 
