e6891d42f6e104e5ed17ab2342e019c94ed23c22
Atul Kumar <akumarofficial4224@gmail.com>
2021-09-28 13:21:05 +0530

    Export type function

--- src/type.js
+++ src/type.js
@@ -1,8 +1,36 @@
-
+const is = require('./utils/is.js');
 
 // Function: Returns the type of array
 function type(array){
-
+    if(Array.isArray(array)){
+        let set = new Set();
+        for(let i = 0; i < array.length; i++){
+            set.add(typeof array[i]);
+        }
+        if(set.size === 1){
+            set = Array.from(set).toString();
+            switch(set){
+                case "boolean":
+                    return "Boolean Array";
+                case "number":
+                    return "Number Array";
+                case "bigint":
+                    return "Bigint Array";
+                case "undefined":
+                    return "Undefined Array";
+                case "symbol":
+                    return "Symbol Array";
+                case "object":
+                    return "Object Array";
+            }
+        }
+        else{
+            return "Mixed Array";
+        }
+    }
+    else{
+        throw new TypeError("Failed to execute 'type': Argument must be an array");
+    }
 }
 
 module.exports = type;
