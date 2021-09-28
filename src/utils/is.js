6ae246c2d872ff466238424be846c06996e35e1f
Atul Kumar <akumarofficial4224@gmail.com>
2021-09-28 11:56:30 +0530

    Write some utility functions

--- src/utils/is.js
+++ src/utils/is.js
@@ -3,4 +3,14 @@ function isNumber(value){
     return !(Math.floor(value) - value) ? true : false;
 }
 
-module.exports = isNumber;
+function isPositive(value){
+    return value >= 0 ? true : false;
+}
+
+function isNegative(value){
+    return value < 0 ? true : false;
+}
+
+module.exports.isNumber = isNumber;
+module.exports.isPositive = isPositive;
+module.exports.isNegative = isNegative;
