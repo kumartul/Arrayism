2581b955b8d05d5837b80dcec959f22bb3b220c1
Atul Kumar <akumarofficial4224@gmail.com>
2021-09-28 11:56:14 +0530

    Write a function to get the element at any index

---
+++ src/nth.js
@@ -0,0 +1,24 @@
+const is = require('./utils/is.js');
+
+// Function: Returns the element at the nth index of an array
+// If n is negative, then the element from the last is returned
+function nth(array, n){
+    if(Array.isArray(array)){
+        if(Number.isInteger(n)){
+            if(is.isPositive(n)){
+                return array[n];
+            }
+            else{
+                return array[array.length + n];
+            }
+        }
+        else{
+            throw new TypeError("Failed to execute 'nth': Position must be an integer");
+        }
+    }
+    else{
+        throw new TypeError("Failed to execute 'nth': First argument must be an array");
+    }
+}
+
+module.exports = nth;
