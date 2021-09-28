d3beab55dfd367e5f33624370fd5b86a39737b9b
Atul Kumar <akumarofficial4224@gmail.com>
2021-09-28 10:45:11 +0530

    Write a function that will clone an array

---
+++ src/clone.js
@@ -0,0 +1,11 @@
+// Function: Clones an array
+function clone(array){
+    if(Array.isArray(array)){
+        return [...array];
+    }
+    else{
+        throw new TypeError("Failed to execute 'clone': Argument must be an array")
+    }
+}
+
+module.exports = clone;
