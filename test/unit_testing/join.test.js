dba4ecc5d3f6a8499e4c5e8e1c84894a630cff0b
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-02 16:27:46 +0530

    Successfully tested both the join methods

---
+++ test/unit_testing/join.test.js
@@ -0,0 +1,9 @@
+const join = require('../../src/join.js');
+
+test('joins one or more element(s) to the array', () => {
+    expect(join.join([1, 2, 3], 6, "Foo", "Bar")).toEqual([1, 2, 3, 6, "Foo", "Bar"]);
+});
+
+test('joins only the elements which are not present in the array', () => {
+    expect(join.joinDiff([1, 2, 3], 6, 1, 2, "Foo", "Bar")).toEqual([1, 2, 3, 6, "Foo", "Bar"]);
+});
