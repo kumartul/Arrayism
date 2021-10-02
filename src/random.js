4edcea40d08ca42cf888bfad0d026330134dcfb4
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-02 09:54:52 +0530

    Write a function to return a random value from an array

---
+++ src/random.js
@@ -0,0 +1,25 @@
+// Function: Returns random value(s) from an array
+function random(array, length=1){
+    if(Array.isArray(array)){
+        if(Number.isInteger(length)){
+            if(length === 1){
+                return array[Math.floor(Math.random() * array.length)];
+            }
+            else{
+                let randomArr = [];
+                for(let i = 0; i < array.length; i++){
+                    randomArr.push(Math.floor(Math.random() * array.length));
+                }
+                return randomArr;
+            }
+        }
+        else{
+            throw new TypeError("Failed to execute 'random': Length must be an integer");
+        }
+    }
+    else{
+        throw new TypeError("Failed to execute 'random': First argument must be an array");
+    }
+}
+
+module.exports = random;
