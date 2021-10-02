4b68c1a0f1dfffe854bc71838ac93d978d97429a
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-02 16:46:00 +0530

    Successfully tested distance

---
+++ test/unit_testing/distance.test.js
@@ -0,0 +1,5 @@
+const distance = require('../../src/distance.js');
+
+test('returns the distance between two indices', () => {
+    expect(distance(["Foo", 1, 4, "Bar", true], 1, true)).toBe(3);
+});
