e22705af4fccbf5b6b64fd967185c95c3e2cd19b
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-05 12:22:44 +0530

    Successfully tested zipObj

---
+++ test/unit_testing/zipObj.test.js
@@ -0,0 +1,6 @@
+const zipObj = require('../../src/zipObj.js');
+
+test('Zips the array in an object', () => {
+    expect(zipObj(["A", "B"], ["Alpha", "Beta"])).toEqual({ "A": "Alpha", "B": "Beta" });
+});
+
