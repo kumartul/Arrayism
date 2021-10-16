ea50412c6d25ea28eb852456d2f530b5a30de505
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-16 19:01:46 +0530

    Write a function to count the number of dimensions in an array

---
+++ src/dimension.js
@@ -0,0 +1,19 @@
+// Function: Counts the number of dimensions in an array
+function dimension(array){
+    if(Array.isArray(array)){
+        let dimension = 1;
+        function iterate(arr){
+            arr.forEach(ar => {
+                if(Array.isArray(ar)){
+                    dimension++;
+                    iterate(ar);
+                }
+            })
+        }
+        iterate(array);
+        return dimension;
+    }
+    else{
+        throw new TypeError("Failed to execute 'dimension': Argument must be an array");
+    }
+}
