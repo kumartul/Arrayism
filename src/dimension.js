5f44f8c20b7928dd2a5fee51ef93cd2cd0607d3b
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-22 22:01:48 +0530

    Move the function declaration and variable declaration to top

--- src/dimension.js
+++ src/dimension.js
@@ -1,15 +1,15 @@
 // Function: Counts the number of dimensions in an array
 function dimension(array){
+    let dimension = 1;
+    function iterate(arr){
+        arr.forEach(ar => {
+            if(Array.isArray(ar)){
+                dimension++;
+                iterate(ar);
+            }
+        });
+    }
     if(Array.isArray(array)){
-        let dimension = 1;
-        function iterate(arr){
-            arr.forEach(ar => {
-                if(Array.isArray(ar)){
-                    dimension++;
-                    iterate(ar);
-                }
-            });
-        }
         iterate(array);
         return dimension;
     }
