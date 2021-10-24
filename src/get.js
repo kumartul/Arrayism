e2d7c1f6dd2168f6d0abe793ff8e8a700280fa06
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-24 12:21:48 +0530

    Lint the code

--- src/get.js
+++ src/get.js
@@ -1,7 +1,7 @@
 // Function: Gets all the value from the array and returns it
 function getValues(array){
     if(Array.isArray(array)){
-        array.forEach(elem => console.log(elem))
+        array.forEach(elem => console.log(elem));
     }
     else{
         throw new TypeError("Failed to execute 'getValues': Argument must be an array");
