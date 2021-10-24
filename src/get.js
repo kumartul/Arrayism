675565842cff2198bee4da3d10ab81df36b0de3c
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-24 11:50:36 +0530

    Use forEach loop instead of for loop

--- src/get.js
+++ src/get.js
@@ -1,13 +1,12 @@
 // Function: Gets all the value from the array and returns it
 function getValues(array){
     if(Array.isArray(array)){
-        for(let i = 0; i < array.length; i++){
-            console.log(array[i]);
-        }
+        array.forEach(elem => console.log(elem))
     }
     else{
         throw new TypeError("Failed to execute 'getValues': Argument must be an array");
     }
 }
+
 
 module.exports = getValues;
